b7a9c95a1c2b50f9bb5141ac1edb54dfde625d0d
Howard P. Logsdon <howard@hplogsdon.com>
2014-07-28 11:35:06 -0600

    Cleanup vagrant user creation to not fail on existing users

--- script/centos/vagrant.sh
+++ script/centos/vagrant.sh
@@ -1,24 +1,30 @@
+#!/bin/bash
+
 date > /etc/vagrant_box_build_time
 
-if ! id -u vagrant >/dev/null 2>&1; then
-    # Vagrant user
-    /usr/sbin/groupadd vagrant
-    /usr/sbin/useradd vagrant -g vagrant -G sudo -d /home/vagrant --create-home
-    echo "vagrant:vagrant" | chpasswd
+id -u vagrant 2>&1 >/dev/null
+if [ $? -ne 0 ]; then
+    echo "Creating vagrant user"
+    /usr/sbin/useradd -d /home/vagrant -m -p vagrant vagrant
 fi
 
 # Set up sudo.  Be careful to set permission BEFORE copying file to sudoers.d
+echo "Adding vagrant user to sudoers file"
 ( cat <<'EOP'
-%vagrant ALL=NOPASSWD:ALL
+vagrant ALL=NOPASSWD:ALL
 EOP
 ) > /tmp/vagrant
 chmod 0440 /tmp/vagrant
-mv /tmp/vagrant /etc/sudoers.d/
+mv /tmp/vagrant /etc/sudoers.d/vagrant
 
 # Install vagrant keys
-mkdir /home/vagrant/.ssh
-chmod 700 /home/vagrant/.ssh
-cd /home/vagrant/.ssh
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
-chmod 600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant /home/vagrant/.ssh
+if [ -d /home/vagrant ]; then
+    echo "Installing vagrant ssh keys"
+    mkdir -vp /home/vagrant/.ssh
+    chmod 700 /home/vagrant/.ssh
+    curl -sk https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/vagrant/.ssh/authorized_keys
+    chmod 600 /home/vagrant/.ssh/authorized_keys
+    chown -R vagrant /home/vagrant/.ssh
+else
+    echo "WARN: vagrant user home directory does not exist?"
+fi
