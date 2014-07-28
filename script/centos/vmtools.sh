8542c719714b0f1c13dc1639df2cd97f82366f1b
Howard P. Logsdon <howard@hplogsdon.com>
2014-07-28 16:57:26 -0600

    Use RHEL vmware tools package from vmware repo

--- script/centos/vmtools.sh
+++ script/centos/vmtools.sh
@@ -3,24 +3,30 @@
 if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
     echo "Installing VMware Tools"
 
-    # Make sure perl is available
-    yum install perl gcc make kernel-headers kernel-devel fuse-libs -y
+    rpm --import http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-DSA-KEY.pub
+    rpm --import http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub
+( cat <<'EOP'
+[vmware-tools]
+name=VMware Tools
+baseurl=http://packages.vmware.com/tools/esx/5.1latest/rhel6/\$basearch
+enabled=1
+gpgcheck=1
+EOP
+) > /tmp/vmware-tools
+    chmod 644 /tmp/vmware-tools
+    mv -v /tmp/vmware-tools /etc/yum.repos.d/vmware-tools.repo
 
-    # Mount the disk image
-    cd /tmp
-    mkdir /tmp/isomount
-    mount -t iso9660 -o loop /home/vagrant/linux.iso /tmp/isomount
+    yum -y install vmware-tools-hgfs vmware-tools-esx-nox
 
-    # Install the drivers
-    cp /tmp/isomount/VMwareTools-*.gz /tmp
-    tar -zxvf VMwareTools*.gz
-    ./vmware-tools-distrib/vmware-install.pl -d
-
-    # Cleanup
-    umount /tmp/isomount
-    rm -rf /tmp/isomount /root/linux.iso VMwareTools*.gz vmware-tools-distrib /home/vagrant/linux.iso
-
+( cat <<'EOP'
+modprobe vmhgfs
+EOP
+) > /tmp/vmware-boot
+    chmod 755 /tmp/vmware-boot
+    mv -v /tmp/vmware-boot /etc/sysconfig/modules/vmhgfs.modules
 fi
+
+
 
 if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
     echo "Installing VirtualBox guest additions"
